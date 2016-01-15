4dca6c7715a6fab3f1b26e63f6f2bfe588f4d5d0
arnav-gudibande <Arnav_Gudibande@Arnav-Gudibandes-MacBook-Pro.local>
2016-01-14 21:09:47 -0800

    Added more features
    1.  Look through API (Graphics2D/Graphics classes), identify method
    that can be used to translate graphical context.  Write the line of
    code necessary to translate our graphical context to the center of the
    clock.
    2.  Write code that draws the outside border of the clock.
    3.  Also in the API, look up the class titled GregorianCalendar.  Write
    code that will pull the current time (hours, minutes, seconds) from the
    internal clock of the machine, store them to variables.

--- ClockComponent.java
+++ ClockComponent.java
@@ -1,28 +1,40 @@
+import javax.swing.JComponent;//importing the neccessary componnents
+import java.awt.*;
+import java.awt.geom.*;
 
-public class ClockComponent
+public class ClockComponent extends JComponent
 {
     public static final int DIAMETER = 400;
     
+    public ClockComponent(){
+        //
+    }
+    
     public void paintComponent(Graphics g){
-       Graphics2D g2 = (Graphics2D g);
+       Graphics2D g2 = (Graphics2D) g;
+       g2.translate(350,350);
        //draw the clock face
-       drawClockFace(g2);
        //draw the clock hands in current time
        drawClockHands(g2);
     }
     
-    public drawClockFace(Graphics2D g2){
+    public static void drawClockFace(Graphics2D g2){
        //draw the outside border
        //draw tick marks along inner perimeter
        //draw the numbers in the proper position
-       
-        
+       Ellipse2D.Double border = new Ellipse2D.Double(0,0,DIAMETER,DIAMETER);
+       g2.setStroke(new BasicStroke(2));
+       g2.draw(border);  
     
-    
+       /*
+        * write a for loop that enumerates around the circle
+        * at every interval, draw a tick line that is perpendicular to the edge of the circle
+        * stop when you go fully around the circle
+        */
     
     }
     
-    public drawClockHands(Graphics2D g2){
+    public void drawClockHands(Graphics2D g2){
       
         //this is the method for animating the clock hands
     
--- MainEC.java
+++ MainEC.java
@@ -1,10 +1,28 @@
+import javax.swing.JFrame;//imports the jframe components
+import java.util.Scanner;//imports the scanner components
+import java.awt.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 public class MainEC
 {
-    public static void main(Strings[] args){
+    public static void main(String[] args){
         JFrame clockFrame = new JFrame("Clock");
-        clockFrame.setSize(clockComponent.DIAMETER*1.75, clockComponent.DIAMETER*1.75);
-        clockComponent c = new ClockComponent();
-        clockFrame.add(c);
+        int size = ClockComponent.DIAMETER * (int) 1.75;
+        clockFrame.setSize(size,size);
+        ClockComponent cF = new ClockComponent(50);
+        clockFrame.add(cF);
         clockFrame.setVisible(true);
+        getTime();
+    }
+    
+    public static void getTime(){
+        Calendar calendar = new GregorianCalendar();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        int second = calendar.get(Calendar.SECOND);
+        System.out.print(hour);
+        System.out.print(minute);
+        System.out.print(second);
+        System.out.println("");
     }
 }
